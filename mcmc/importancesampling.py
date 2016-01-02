d6cdb5b987526752e80e552fdc27415d0c0db6bc
Adam Wheeler <wheeler.883@osu.edu>
2016-01-01 19:08:01 -0500

    importance sampling for orientation prior

---
+++ mcmc/importancesampling.py
@@ -0,0 +1,20 @@
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description="apply prior")
+parser.add_argument('chain_file', type=str)
+parser.add_argument('output_file', type=str)
+args = parser.parse_args()
+
+chain = np.load(args.chain_file)
+
+accepted = []
+for theta in chain:
+    gamma = np.sqrt(theta[2]**2  + theta[3]**2)
+    if gamma > 1:
+        pass
+    if np.random.rand() < gamma:
+        accepted.append(theta)
+
+
+np.save(args.output_file, np.array(accepted))
