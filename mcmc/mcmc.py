54e9f543b3b82fe762a3e56763f30028feb96bfb
Adam Wheeler <wheeler.883@osu.edu>
2016-01-05 12:28:50 -0500

    tighten parameter constrains further

--- mcmc/mcmc.py
+++ mcmc/mcmc.py
@@ -32,9 +32,8 @@ def lnprob(theta, data, r_psf):
     params.fromArray(theta)
 
     # use g < .99 instead of g < 1 because fft can't handle g~1
-    if params.g1d**2 + params.g2d**2 > .97 \
-       or params.g1b**2 + params.g2b**2 > .97\
-       or params.g1s**2 + params.g2s**2 > .97:
+    if np.sqrt(params.g1d**2 + params.g2d**2) > .95 \
+       or np.sqrt(params.g1b**2 + params.g2b**2) > .95\
         return -np.inf
 
     gal = model.egg(params, match_image_size=data)
