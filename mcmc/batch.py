457bfd20d787d3ac2633fad19f51d9e375b39983
Adam Wheeler <wheeler.883@osu.edu>
2016-05-12 00:40:21 -0400

    typo

--- mcmc/batch.py
+++ mcmc/batch.py
@@ -9,9 +9,9 @@ import priors
 def valid(theta):
     """returns True iff all shear vectors have mag <1"""
     p = True
-    p = p and theta[2]**2 + theta[3]**2 < 1
-    p = p and theta[6]**2 + theta[7]**1 < 1
-    p = p and theta[8]**2 + theta[9]**2 < 1
+    p = p and theta.g1d**2 + theta.g2d**2 < 1
+    p = p and theta.g1b**2 + theta.g2b**1 < 1
+    p = p and theta.g1s**2 + theta.g2s**2 < 1
     return p
 
 SNR = 50
