def145dc5a092ffccd303ff89d1c89fe267b7a2c
Adam Wheeler <wheeler.883@osu.edu>
2017-01-23 16:13:19 -0500

    add batchdraw.py and galplots.py

---
+++ mcmc/batchdraw.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+import drawcorner
+
+for dir in os.listdir('.'):
+    os.chdir(dir)
+    print(os.getcwd())
+
+    for b in ['mag.single', 'mag.double', 'shear.single', 'shear.double']:
+        chain = np.load(b + '.chain.npy')
+        kormendy = np.load(b + '.kormendy.npy')
+        orientation = np.load(b + '.orientation.npy')
+        
+        with open(b + '.stats.p', 'rb') as f:
+            stats = pickle.load(f)
+
+        print(stats['mask'])
+
+        fig = drawcorner.make_figure(chain, 
+                                     stats['fiducial_params'].toArray(stats['mask']),
+                                     mask=stats['mask'])
+        fig.savefig(b + '.png')
+
+        fig = drawcorner.make_figure(chain, 
+                                     stats['fiducial_params'].toArray(stats['mask']), 
+                                     weights=kormendy*orientation,
+                                     mask=stats['mask'])
+        fig.savefig(b + '.withpriors.png')        
+        
+    os.chdir('..')
