da8413f4e9dc75aa06b07fc48668a97efbc5b241
Adam Wheeler <adamwheeler@Adams-MacBook-Pro.local>
2017-05-05 15:57:34 -0400

    minimal viable test_errorbars.py

--- mcmc/test_errorbars.py
+++ mcmc/test_errorbars.py
@@ -4,16 +4,37 @@ import mcmc
 import model
 import calculate_mode
 
-nwalkers = 100
-nsample = 200
-nburnin = 30
+
+nwalkers = 200
+nsample = 100
+nburnin = 50
 
 mask = [True] + [False]*10
 dual_band = False
 trueParams = model.EggParams()
 
-data, var = mcmc.generate_data(trueParams, dual_band)
 
-sampler, stats = mcmc.run_chain(data, var, trueParams, nwalkers, nburnin,
-                                nsample, mask=mask, dual_band=dual_band)
-print(calculate_mode.median(sampler.flatchain()))
+trials = 20
+qs = []
+for _ in range(trials):
+    data, var = mcmc.generate_data(trueParams, dual_band)
+
+    sampler, stats = mcmc.run_chain(data, var, trueParams, nwalkers, nburnin,
+                                    nsample, mask=mask, dual_band=dual_band)
+    print("autocorrelation time: {}".format(stats['autocorrelation_time']))
+    print("acceptance fraction: {}".format(stats["acceptance_fraction"]))
+    q = calculate_mode.median(sampler.flatchain)[0]
+    qs.append(q)
+    print("{} (+{}) (-{})".format(q[1], q[1]-q[0], q[2]-q[1]))
+    print("")
+    
+qs = np.array(qs)
+
+q = sum(qs)/trials
+print("{} (+{}) (-{})".format(q[1], q[1]-q[0], q[2]-q[1]))
+
+
+# import drawcorner
+# import matplotlib.pyplot as plt
+# drawcorner.make_figure(sampler.flatchain, [3.0])
+# plt.show()
